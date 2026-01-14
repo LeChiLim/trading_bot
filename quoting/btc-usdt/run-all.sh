c86be69fef87c0a598fbf30bae93c9317eccb065
LeChiLim <limlechi@gmail.com>
2026-01-14 19:20:35 +0800

    add run-all bash script

---
+++ quoting/btc-usdt/run-all.sh
@@ -0,0 +1,6 @@
+exec python3 /home/asus_laptop/projects/trading_bot/quoting/btc-usdt/quote_binance_btc-usd.py &
+exec python3 /home/asus_laptop/projects/trading_bot/quoting/btc-usdt/quote_cryptocoms_btc-usd.py & 
+exec python3 /home/asus_laptop/projects/trading_bot/quoting/btc-usdt/quote_kraken_btc-usd.py &
+exec python3 /home/asus_laptop/projects/trading_bot/quoting/btc-usdt/quote_kucoin_btc-usd.py &
+
+wait
